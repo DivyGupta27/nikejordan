72c3ee10cc67c21b4bb7e4e6fb7846f72d5eb760
DivyGupta27 <divygupta2002@gmail.com>
2025-05-10 20:36:39 +0530

    svgdfsg

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,16 +3,17 @@ import React from "react";
 import Slider from "react-slick";
 import img1 from '../assets/img1.jpg'
 import img2 from '../assets/img2.jpg'
-import img3 from '../assets/img3.jpg'
+// import img3 from '../assets/img3.jpg'
+// import img3 from '../assets/img3.jpg'
 
-import img5 from '../assets/img5.jpg'
-// import img6 from '../assets/img6.jpg'
-import img7 from '../assets/img7.jpg'
+// import img5 from '../assets/img5.jpg'
+import img6 from '../assets/img6.jpg'
+// import img7 from '../assets/img7.jpg'
 import img8 from '../assets/img8.jpg'
 // import img9 from '../assets/img9.jpg'
 import img0 from '../assets/img0.jpg'
 
-const images = [img1, img2, img3,img5,img7,img8,img0];
+const images = [img1, img2,img6 ,img8,img0];
 const Carousel = () => {
   const settings = {
     dots: true,
