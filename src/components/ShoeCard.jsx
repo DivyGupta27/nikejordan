67260211b7f8fdd2cb974e823afab25cb99af523
DivyGupta27 <divygupta2002@gmail.com>
2025-05-11 14:48:07 +0530

    pointer

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -69,13 +69,13 @@ const ShoeCard = ({ name, category, price, color, gender, image, inStock}) => {
       <div className='flex gap-2 mt-auto'>
         <button
           onClick={handleAdd}
-          className="mt-4 w-full bg-gray-200 px-4 py-2 rounded hover:bg-gray-300"
+          className="mt-4 w-full bg-gray-200 px-4 py-2 cursor-pointer rounded hover:bg-gray-300"
         >
           Add to Cart
         </button>
         <button
           onClick={handleBuy}
-          className="mt-4 w-full bg-black text-white px-4 py-2 rounded hover:bg-gray-800"
+          className="mt-4 w-full bg-black text-white px-4 py-2 cursor-pointer rounded hover:bg-gray-800"
         >
           Buy Now
         </button>
